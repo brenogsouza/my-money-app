d99492358fcde14cfa5dca357600679f075f12ff
brenodev <breno.souza.developer@gmail.com>
2019-08-26 21:44:46 -0300

    mapeando objeto e criando schema do banco

---
+++ backend/src/api/billingCycle/billingCycle.js
@@ -0,0 +1,57 @@
+const restful = require("node-restful");
+const mongoose = restful.mongoose;
+
+// ciclo de pagamento
+const creditSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  value: {
+    type: Number,
+    min: 0,
+    require: true
+  }
+});
+
+// ciclo de debito
+const debtSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  value: {
+    type: Number,
+    min: 0,
+    require: true
+  },
+  status: {
+    type: String,
+    require: false,
+    uppercase: true,
+    enum: ["PAGO", "PENDENTE", "AGENDADO"]
+  }
+});
+
+const billingCycleSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  month: {
+    type: Number,
+    min: 1,
+    max: 12,
+    require: true
+  },
+  year: {
+    type: Number,
+    min: 1970,
+    max: 2100,
+    require: true,
+    credits: [creditSchema],
+    debts: [debtSchema]
+  }
+});
+
+module.exports = restful.model("billingCycle", billingCycleSchema);

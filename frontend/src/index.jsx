96cdb7664477635af48bb16dc76692016577bbf7
Brenodev <breno.souza.developer@gmail.com>
2019-08-27 13:08:11 -0300

    criado componente app e index

---
+++ frontend/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//CUSTOM COMPONENTS
+import App from "./main/app";
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <h1>ola mundo</h1>
+    </div>
+  );
+};
+export default App;

2b26ff512881f17b9d654fd959b56c3584f4597e
Brenodev <breno.souza.developer@gmail.com>
2019-08-27 13:51:26 -0300

    definindo componente MENU

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
+//CUSTOM COMPONENT
+import MenuItem from "./menuItem";
+import MenuTree from "./menuTree";
+
 const Menu = props => {
-  return <ul className="sidebar-menu"></ul>;
+  return (
+    <ul className="sidebar-menu">
+      <MenuItem path="#/" label="Dashboard" icon="dashboad" />
+      <MenuTree label="Cadastro" icon="edit">
+        <MenuItem
+          path="#billingCycles"
+          label="Ciclos de Pagamento"
+          icon="usd"
+        />
+      </MenuTree>
+    </ul>
+  );
 };
 export default Menu;

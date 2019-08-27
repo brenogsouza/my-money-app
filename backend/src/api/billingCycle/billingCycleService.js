a3a1952c8f86702f0daa009c8c8d3dc6ae5b41ce
Brenodev <breno.souza.developer@gmail.com>
2019-08-27 10:45:38 -0300

    obtendo o sumario de pagamento

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -14,6 +14,36 @@ BillingCycle.route("count", (req, res, next) => {
   });
 });
 
+BillingCycle.route("summary", (req, res, next) => {
+  BillingCycle.aggregate(
+    [
+      {
+        $project: {
+          credit: { $sum: "$credits.value" },
+          debt: { $sum: "$debts.value" }
+        }
+      },
+      {
+        $group: {
+          _id: null,
+          credit: { $sum: "$credit" },
+          debt: { $sum: "$debt" }
+        }
+      },
+      {
+        $project: { _id: 0, credit: 1, debt: 1 }
+      }
+    ],
+    (error, result) => {
+      if (error) {
+        res.status(500).json({ errors: [error] });
+      } else {
+        res.json(result[0] || { credit: 0, debt: 0 });
+      }
+    }
+  );
+});
+
 BillingCycle.updateOptions({ new: true, runValidators: true });
 
 module.exports = BillingCycle;

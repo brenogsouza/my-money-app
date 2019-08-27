34f63ba37450e78984945afd87ae62e99540493b
Brenodev <breno.souza.developer@gmail.com>
2019-08-27 11:29:06 -0300

    middleware query parser int

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,23 +1,21 @@
 // configuração do servidor
-
 // definição da porta
 const port = 3003;
-
 // middleware retorna o corpo da requisição todo pronto
 const bodyParser = require("body-parser");
-
 // restorna um singleton, ou seja instancia única
 const expres = require("express");
-
 // constante referente ao server, que retorna um novo servidor
 const server = expres();
-
 // constante referente ao cabeçalho
 const allowCors = require("./cors");
+// constante referente ao queryInt
+const queryParser = require("express-query-int");
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(allowCors);
+server.use(queryParser());
 
 //configurando a PORTA
 server.listen(port, function() {

187221014a79540d3a1cd2b595b62ae4c6c0b941
Brenodev <breno.souza.developer@gmail.com>
2019-08-27 14:25:41 -0300

    componente footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = props => (
+  <footer className="main-footer">
+    <strong>Copyright &copy; By React</strong>
+  </footer>
+);
+
+export default Footer;
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,12 +4,17 @@ import React from "react";
 // CUSTOM COMPONENTS
 import Header from "../common/template/header";
 import Sidebar from "../common/template/sidebar";
+import Footer from "../common/template/footer";
 
 const App = props => {
   return (
     <div className="wrapper">
       <Header />
       <Sidebar />
+      <div className="content-wrapper">
+        <h1>Conteúdo</h1>
+      </div>
+      <Footer />
     </div>
   );
 };

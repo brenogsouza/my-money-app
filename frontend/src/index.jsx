80895d68d069c5c3bb57253375af51416457d86b
Brenodev <breno.souza.developer@gmail.com>
2019-08-27 15:57:33 -0300

    configurando o redux e adicionando o redux-dev-tools

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 //CUSTOM COMPONENTS
 import App from "./main/app";
+import reducers from "./main/reducers";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);

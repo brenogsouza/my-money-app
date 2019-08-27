d42e336311b9aaeafbb3aafe30c1748d668122ca
brenodev <breno.souza.developer@gmail.com>
2019-08-26 21:18:18 -0300

    realizado configuração do server

---
+++ backend/src/config/server.js
@@ -0,0 +1,20 @@
+// configuração do servidor
+
+// definição da porta
+const port = 3003;
+
+// middleware retorna o corpo da requisição todo pronto
+const bodyParser = require("body-parser");
+
+// restorna um singleton, ou seja instancia única
+const expres = require("express");
+
+// constante referente ao server, que retorna um novo servidor
+const server = expres();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.listen(port, function() {
+  console.log(`BACKEND is runnin on port ${port}.`);
+});

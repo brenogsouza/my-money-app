17856cda8490857c1e6b355d548f53e18978ef93
Brenodev <breno.souza.developer@gmail.com>
2019-08-28 08:49:46 -0300

    criado componente TABHEADER

--- frontend/src/billingCycles/billingCycles.jsx
+++ frontend/src/billingCycles/billingCycles.jsx
@@ -6,6 +6,7 @@ import Content from "../common/template/content";
 import Tabs from "../common/tab/tabs";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
+import TabHeader from "../common/tab/tabHeader";
 
 class BillingCycles extends Component {
   render() {
@@ -14,7 +15,12 @@ class BillingCycles extends Component {
         <ContentHeader title="Ciclos de Pagamentos" small="Cadastro" />
         <Content>
           <Tabs>
-            <TabsHeader></TabsHeader>
+            <TabsHeader>
+              <TabHeader label="Listar" icon="bars" target="tabList" />
+              <TabHeader label="Incluir" icon="plus" target="tabCreate" />
+              <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+              <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
+            </TabsHeader>
             <TabsContent></TabsContent>
           </Tabs>
         </Content>

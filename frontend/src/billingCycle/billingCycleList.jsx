4241e5405575e22a72625302575110fec387e964
Brenodev <breno.souza.developer@gmail.com>
2019-08-28 15:37:23 -0300

    exibir aba de alterar ciclo de pagamento

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { getList } from "./billingCycleActions";
+import { getList, showUpdate } from "./billingCycleActions";
 
 class BillingCycleList extends Component {
   componentWillMount() {
@@ -15,6 +15,14 @@ class BillingCycleList extends Component {
         <td>{bc.name}</td>
         <td>{bc.month}</td>
         <td>{bc.year}</td>
+        <td>
+          <button
+            className="btn btn-warning"
+            onClick={() => this.props.showUpdate(bc)}
+          >
+            <i className="fa fa-pencil"></i>
+          </button>
+        </td>
       </tr>
     ));
   }
@@ -29,6 +37,7 @@ class BillingCycleList extends Component {
               <th>Nome</th>
               <th>Mês</th>
               <th>Ano</th>
+              <th>Ações</th>
             </tr>
           </thead>
           <tbody>{this.renderRows()}</tbody>
@@ -43,7 +52,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ getList }, dispatch);
+  bindActionCreators({ getList, showUpdate }, dispatch);
 
 export default connect(
   mapStateToProps,

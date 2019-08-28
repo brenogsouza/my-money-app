f313ac37927c597ede5e16a0939bd383ada32351
Brenodev <breno.souza.developer@gmail.com>
2019-08-28 11:35:00 -0300

    criado action getList e o Reducer BillingCycles

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -3,10 +3,12 @@ import { combineReducers } from "redux";
 //REDUCERS
 import DashboardReducer from "../dashboard/dashboardReducer";
 import tabReducer from "../common/tab/tabReducer";
+import BillingCycleReducer from "../billingCycle/billingCycleReducer";
 
 const rootReducers = combineReducers({
   dashboard: DashboardReducer,
-  tab: tabReducer
+  tab: tabReducer,
+  billingCycle: BillingCycleReducer
 });
 
 export default rootReducers;

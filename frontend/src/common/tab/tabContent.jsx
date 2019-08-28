251a3dd45d0aa1e89a576aadbab873bd19b455a2
Brenodev <breno.souza.developer@gmail.com>
2019-08-28 09:31:47 -0300

    connectando componente TABCONTENT e criado navegação para as tabs

---
+++ frontend/src/common/tab/tabContent.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+// import { bindActionCreators } from "redux";
+
+class TabContent extends Component {
+  render() {
+    const selected = this.props.tab.selected === this.props.id;
+    return (
+      <div
+        className={`tab-pane ${selected ? "active" : ""}`}
+        id={this.props.id}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  tab: state.tab
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(TabContent);

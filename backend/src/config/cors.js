8b57b8fdb93901f883c8167f410645de3e2ae3c3
Brenodev <breno.souza.developer@gmail.com>
2019-08-29 15:27:18 -0300

    criado middleware para verificação do token e alterado o cors para aceitar authorization

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -7,7 +7,8 @@ module.exports = (req, res, next) => {
   );
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept",
+    "Authorization"
   );
   next();
 };
